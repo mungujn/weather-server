7f9943abe1e11d385920df267187f92a92d7a822
Mungujakisa Nickson <nicksonmjk@gmail.com>
2018-12-01 12:26:50 +0300

    fixed weather tests

--- weather/mock_services/weather_service_test.go
+++ weather/mock_services/weather_service_test.go
@@ -1,18 +1,58 @@
 package mock_services_test
-package mock_services_test
 
 import (
-	"time"
-	"golang.org/x/net/context"
 	"fmt"
 	"testing"
+	"time"
 
-	pbmock "github.com/mungujn/weather/common/mock_services"
-	pb "github.com/mungujn/weather/common/services"
+	"golang.org/x/net/context"
+
+	pbmock "github.com/mungujn/weather/weather/mock_services"
+	pb "github.com/mungujn/weather/weather/services"
 
 	"github.com/golang/mock/gomock"
 	"github.com/golang/protobuf/proto"
 )
+
+var client pb.WeatherServiceClient
+
+//TestWeatherService tests rpc methods in the WeatherService
+func TestWeatherService(t *testing.T) {
+	// set up
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	client := getClient(ctrl)
+
+	// test
+	response, err := client.GetWeather(ctx, &pb.LocationAndDate{Location: "location", Date: "today"})
+
+	// assert
+	if err != nil || response.Temperature != 28 || response.Location != "kampala" || response.Date != "today" {
+		t.Errorf("Get weather mocking failed")
+	}
+	t.Log("Response : ", response)
+}
+
+// get rpc client
+func getClient(ctrl *gomock.Controller) pb.WeatherServiceClient {
+	if client == nil {
+		newClient := pbmock.NewMockWeatherServiceClient(ctrl)
+
+		req := &pb.LocationAndDate{Location: "location", Date: "today"}
+
+		newClient.EXPECT().GetWeather(
+			gomock.Any(),
+			&rpcMsg{msg: req},
+		).Return(&pb.Weather{Temperature: 28, Location: "kampala", Date: "today"}, nil)
+
+		return newClient
+	}
+	return client
+}
 
 type rpcMsg struct {
 	msg proto.Message
@@ -30,32 +70,4 @@ func (r *rpcMsg) Matches(msg interface{}) bool {
 
 func (r *rpcMsg) String() string {
 	return fmt.Sprintf("is %s", r.msg)
-}
-
-//TestWeatherService tests rpc methods in the WeatherService
-func TestWeatherService(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockWeatherClient := pbmock.NewMockWeatherServiceClient(ctrl)
-
-	req := &pb.LocationAndDate{Location: "location", Date: "today"}
-	mockWeatherClient.EXPECT().GetWeather(
-		gomock.Any(),
-		&rpcMsg{msg: req},
-	).Return(&pb.Weather{Temperature: 28, Location: "kampala", Date: "today"}, nil)
-
-	testGetWeather(t, mockWeatherClient)
-}
-
-func testGetWeather(t *testing.T, client pb.WeatherServiceClient) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	response, err := client.GetWeather(ctx, &pb.LocationAndDate{Location: "location", Date: "today"})
-
-	if err != nil || response.Temperature != 28 || response.Location != "kampala" || response.Date != "today" {
-		t.Errorf("Get weather mocking failed")
-	}
-	t.Log("Response : ", response)
 }
